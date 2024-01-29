34043d7abfd6ce9bf8efc848cb3e4ef63fbd68f7
ebayasgalan <eric.bayasgalan@gmail.com>
2024-01-29 09:31:28 -0600

    Add height and width to Floor component

--- Floor.js
+++ Floor.js
@@ -4,6 +4,8 @@ import Images from './assets/Images';
 
 export default class Floor extends Component {
     render() {
+        const width = this.props.body.bounds.max.x - this.props.body.bounds.min.x;
+        const height = this.props.body.bounds.max.y - this.props.body.bounds.min.y;
         const x = this.props.body.position.x - width / 2;
         const y = this.props.body.position.y - height / 2;
 
@@ -15,6 +17,8 @@ export default class Floor extends Component {
                     position: "absolute",
                     left: x,
                     top: y,
+                    width: width,
+                    height: height,
                     overflow: 'hidden',
                     flexDirection: 'row'
                 }}>
