5fed4edc37251eb06e063004af968e10890d5163
ebayasgalan <eric.bayasgalan@gmail.com>
2024-02-03 11:20:45 -0600

    Add styling and update logic for App

--- App.js
+++ App.js
@@ -7,54 +7,155 @@ import Floor from './Floor';
 import Constants from './Constants';
 import Images from './assets/Images';
 
-class Bird extends Component {
-  render() {
-    const { position } = this.props;
-    const birdStyle = {
-      position: 'absolute',
-      width: 50,
-      height: 50,
-      left: position[0],
-      top: position[1],
-    };
-
-    return (
-      <Image
-        style={birdStyle}
-        source={require('./assets/bird.png')}
-      />
-    );
-  }
-}
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      running: true,
-      birdPosition: [50, 250], // Initial position of the bird
-    };
-  }
-
-  render() {
-    return (
-      <GameEngine
-        style={styles.container}
-        systems={[]}
-        entities={{
-          bird: { position: this.state.birdPosition, renderer: <Bird /> },
-        }}
-        running={this.state.running}
-      />
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#87CEEB', 
+      flex: 1,
+      backgroundColor: '#fff',
   },
+  backgroundImage: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      width: Constants.MAX_WIDTH,
+      height: Constants.MAX_HEIGHT
+  },
+  gameContainer: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+  },
+  gameOverText: {
+      color: 'white',
+      fontSize: 48,
+      fontFamily: '04b_19'
+  },
+  gameOverSubText: {
+      color: 'white',
+      fontSize: 24,
+      fontFamily: '04b_19'
+  },
+  fullScreen: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: 'black',
+      opacity: 0.8,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  score: {
+      position: 'absolute',
+      color: 'white',
+      fontSize: 72,
+      top: 50,
+      left: Constants.MAX_WIDTH / 2 - 20,
+      textShadowColor: '#444444',
+      textShadowOffset: { width: 2, height: 2},
+      textShadowRadius: 2,
+      fontFamily: '04b_19'
+  },
+  fullScreenButton: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      flex: 1
+  }
 });
 
-export default App;
+export default class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            running: true,
+            score: 0,
+        };
+
+        this.gameEngine = null;
+
+        this.entities = this.setupWorld();
+    }
+
+    setupWorld = () => {
+        let engine = Matter.Engine.create({ enableSleeping: false });
+        let world = engine.world;
+        world.gravity.y = 0.0;
+
+        let bird = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT / 2, Constants.BIRD_WIDTH, Constants.BIRD_HEIGHT);
+
+        let floor1 = Matter.Bodies.rectangle(
+            Constants.MAX_WIDTH / 2,
+            Constants.MAX_HEIGHT - 25,
+            Constants.MAX_WIDTH + 4,
+            50,
+            { isStatic: true }
+        );
+
+        let floor2 = Matter.Bodies.rectangle(
+            Constants.MAX_WIDTH + (Constants.MAX_WIDTH / 2),
+            Constants.MAX_HEIGHT - 25,
+            Constants.MAX_WIDTH + 4,
+            50,
+            { isStatic: true }
+        );
+
+
+        Matter.World.add(world, [bird, floor1, floor2]);
+        Matter.Events.on(engine, 'collisionStart', (event) => {
+            var pairs = event.pairs;
+
+            this.gameEngine.dispatch({ type: "game-over"});
+
+        });
+
+        return {
+            floor1: { body: floor1, renderer: Floor },
+            floor2: { body: floor2, renderer: Floor },
+            bird: { body: bird, pose: 1, renderer: Bird},
+        }
+    }
+
+    onEvent = (e) => {
+        if (e.type === "game-over"){
+            this.setState({
+                running: false
+            });
+        } else if (e.type === "score") {
+            this.setState({
+                score: this.state.score + 1
+            })
+        }
+    }
+
+    reset = () => {
+        resetPipes();
+        this.gameEngine.swap(this.setupWorld());
+        this.setState({
+            running: true,
+            score: 0
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image source={Images.background} style={styles.backgroundImage} resizeMode="stretch" />
+
+                {!this.state.running && <TouchableOpacity style={styles.fullScreenButton} onPress={this.reset}>
+                    <View style={styles.fullScreen}>
+                        <Text style={styles.gameOverText}>Game Over</Text>
+                        <Text style={styles.gameOverSubText}>Try Again</Text>
+                    </View>
+                </TouchableOpacity>}
+            </View>
+        );
+    }
+}
