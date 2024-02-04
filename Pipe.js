16130550733de0f0eeb9f44057e882b345423e54
ebayasgalan <eric.bayasgalan@gmail.com>
2024-02-04 10:37:14 -0600

    Add new component

---
+++ Pipe.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { View, Image } from "react-native";
+import Images from './assets/Images';
+
+export default class Pipe extends Component {
+    render() {
+        
+    }
+}
